b5d6fdcab08dd1ec63b3d9f217f7919f775caa9e
d.georgiev.91 <d.georgiev.91@gmail.com>
2015-01-08 19:44:40 +0200

    defined app config

--- Ads.Client/js/app.js
+++ Ads.Client/js/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿'use stict';
+
+angular.module('adsApp', ['ngRoute', 'ngResource'])
+    .constant('baseServiceUrl', 'http://localhost:1337//')
+    .constant('pageSize', 2)
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'home-controller'
+        });
+
+        $routeProvider.when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'login-controller'
+        });
+
+        // TODO: define a route for the register controller
+
+        $routeProvider.otherwise(
+            { redirectTo: '/' }
+        );
+    });
