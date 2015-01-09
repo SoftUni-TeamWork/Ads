4721d2c004ad497273c46711ac1cd82d497c2280
d.georgiev.91 <d.georgiev.91@gmail.com>
2015-01-10 00:58:21 +0200

    Added additianal method for user handling for login

--- Ads.Client/js/services/auth-service.js
+++ Ads.Client/js/services/auth-service.js
@@ -2,53 +2,79 @@
 
 angular.module('adsApp.services', [])
     .factory('authService', [
-        '$http', '$window', 'baseServiceUrl', function ($http, $window, baseServiceUrl) {
-            return {
-                login: function(userData, success, error) {
-                    var request = {
-                        method: 'POST',
-                        url: baseServiceUrl + '/api/user/login',
-                        data: userData
-                    };
+        '$http', '$window', 'baseServiceUrl', function($http, $window, baseServiceUrl) {
 
-                    $http(request).$promise.then(function(data) {
-                        $window.sessionStorage['currentUser'] = JSON.stringify(data);
-                        success(data);
-                    }, error);
-                },
+            function login(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/login',
+                    data: userData
+                };
 
-                logout: function () {
-                    delete $window.sessionStorage['currentUser'];
-                },
+                $http(request).$promise.then(function(data) {
+                    $window.sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }, error);
+            }
 
-                register: function(userData, success, error) {
-                    // TODO
-                },
+            function logout() {
+                delete $window.sessionStorage['currentUser'];
+            }
 
-                getCurrentUser: function() {
-                    // TODO
-                },
+            function register(userData, success, error) {
+                // TODO
+            }
 
-                isAnonymous: function() {
-                    // TODO
-                },
+            function getCurrentUser() {
+                var currentUser = $window.sessionStorage['currentUser'];
 
-                isLoggedIn: function() {
-                    // TODO
-                },
-
-                isNormalUser: function() {
-                    // TODO
-                },
-
-                isAdmin: function() {
-                    // TODO
-                },
-
-                getAuthHeaders: function() {
-                    // TODO
+                if (currentUser) {
+                    return JSON.parse(currentUser);
                 }
             }
 
+            function isAnonymous() {
+                return $window.sessionStorage['currentUser'] === undefined;
+            }
+
+            function isLoggedIn() {
+                return !isAnonymous();
+            }
+
+            function isNormalUser() {
+                var currentUser = getCurrentUser();
+
+                if (currentUser) {
+                    return !currentUser.isAdmin;
+                }
+
+                return false;
+            }
+
+            function isAdmin() {
+                return isLoggedIn() && !isNormalUser();
+            }
+
+            function getAuthHeaders() {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+
+                if (currentUser) {
+                    headers.Authorization = 'Bearer ' + currentUser.access_token;
+                }
+
+                return headers;
+            }
+
+            return {
+                login: login,
+                logout: logout,
+                getCurrentUser: getCurrentUser,
+                isAnonymous: isAnonymous,
+                isLoggedIn: isLoggedIn,
+                isNormalUser: isNormalUser,
+                isAdmin: isAdmin,
+                getAuthHeaders: getAuthHeaders
+            }
         }
     ])
