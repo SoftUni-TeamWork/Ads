af86135bd95815f63cda51a10492d8bca7454939
d.georgiev.91 <d.georgiev.91@gmail.com>
2015-01-11 13:43:22 +0200

    some improvments

--- Ads.Client/js/controllers/app-controller.js
+++ Ads.Client/js/controllers/app-controller.js
@@ -1,13 +1,16 @@
 ﻿'use strict';
 angular.module('adsApp.controllers', [])
     .controller('AppController', [
-        '$scope', '$location', 'authService', 'notifyService', function ($scope, $location, authService, notifyService) {
+        '$scope', '$location', 'authService', 'notifyService', function($scope, $location, authService, notifyService) {
             $scope.authService = authService;
-            $scope.currentLocation = $location.path('/');
             $scope.logout = function() {
                 authService.logout();
                 notifyService.showInfo("Logout successful");
                 $location.path('/');
             }
+
+            $scope.$on('$routeChangeSuccess', function() {
+                $scope.currentLocation = $location.path();
+            });
         }
     ]);
--- Ads.SpaClient/Scripts/controllers/error-404-controller.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-angular.module('app.controllers', [])
-    // Path: /error/404
-    .controller('Error404Controller', [
-        '$scope', '$location', '$window', function ($scope, $location, $window) {
-            $scope.$root.title = 'Error 404: Page Not Found';
-        }
-    ]);
